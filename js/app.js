80e92e15131dba965f0c3d294876f67dc6c37d8f
App Academy Student <aa@Apps-Mac-mini-5.local>
2019-10-22 11:26:16 -0400

    modulize pieGenerator and repoFetch

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const unContainer = document.querySelector(".main-profile-un");
   const reposContainer = document.querySelector(".main-profile-repos");
   const urlContainer = document.querySelector(".main-profile-url");
-  const avatarContainer = document.querySelector(".main-profile-avatar"); 
+  const avatarContainer = document.querySelector(".main-profile-avatar");
 
   const clientId = "Iv1.9e39903e84bf3a07";
   const clientSecret = "0bf1a222c2f3f2a99c08bc84ba5fde728b6e4e41";
@@ -23,11 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const repo_api_call = await fetch(
       `https://api.github.com/users/${owner}/repos?client_id=${clientId}&client_secret=${clientSecret}`
     );
-    console.log(repo_api_call)
-    const repo_data = await repo_api_call.json() || [];
-   
-    console.log("this is the repo data");
-    console.log(repo_data);
+    const repo_data = await repo_api_call.json();
     const repoNames = repo_data.map( repo_datum => 
         repo_datum.name
       )
@@ -39,13 +35,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const fetchRepoStats = async (owner, repo) => {
     const repo_stats_api_call = await fetch(`https://api.github.com/repos/${owner}/${repo}/stats/commit_activity?client_id=${clientId}&client_secret=${clientSecret}`);
-    console.log("this is the repo stats api call")
     console.log(repo_stats_api_call)
     const repo_stats_data = await repo_stats_api_call.json(); 
     console.log(repo_stats_data)
     
     let sum = 0; 
-    console.log(repo_stats_data);
+    // console.log(repo_stats_data);
     repo_stats_data.map( repo_stats_datum => 
       sum += repo_stats_datum.total
     )
@@ -61,7 +56,6 @@ document.addEventListener("DOMContentLoaded", () => {
     reposContainer.innerHTML = `Repos: <span class="main__profile-value">${user.public_repos}</span>`
     urlContainer.innerHTML = `URL: <span class="main__profile-value">${user.html_url}</span>`
     avatarContainer.innerHTML = `<img src="${user.avatar_url}" alt="avatar">`;
-    
 
   }
 
@@ -75,13 +69,15 @@ document.addEventListener("DOMContentLoaded", () => {
       })
     );
 
-    let data = {};
+    let pieData = {};
     for (let i = 0; i < repoNames.length; i++) {
-      data[repoNames[i]] = repoCommits[i];
+      pieData[repoNames[i]] = repoCommits[i];
     }
 
-    //beginning of pie chart end of fetch requests
+    //beginning of pie chart end of fetch requests 
 
+
+    let data = pieData;
     console.log(data);
     return data; 
   };
@@ -90,100 +86,102 @@ document.addEventListener("DOMContentLoaded", () => {
   const pieGenerator = async data => {
     d3.selectAll("#vis > *").remove();
 
-    let width = 500;
-    let height = 500;
-    let margin = 40;
-    let radius = Math.min(width, height) / 2 - margin;
+     let width = 500;
+     let height = 500;
+     let margin = 40;
+     let radius = Math.min(width, height) / 2 - margin;
 
-    var svg = d3
-      .select("#vis")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .append("g")
-      .attr("transform", `translate( ${width / 2},${height / 2})`);
+     var svg = d3
+       .select("#vis")
+       .append("svg")
+       .attr("width", width)
+       .attr("height", height)
+       .append("g")
+       .attr("transform", `translate( ${width / 2},${height / 2})`);
 
-    //  data =  { a: 9, b: 20, c: 30, d: 8, e: 12 };
+     // var data = await { a: 9, b: 20, c: 30, d: 8, e: 12 };
 
-    var color = d3
-      .scaleOrdinal()
-      .domain(data)
-      .range([
-        "#FFF07C",
-        "#80FF72",
-        "#7EE8FA",
-        "#00DBBE",
-        "#00E89E",
-        "#00FCAC",
-        "#d3ac00",
-        "#f7e600",
-        "#a2e800",
-        "#03d300",
-        "#00d176"
-      ]);
+     var color = d3
+       .scaleOrdinal()
+       .domain(data)
+       .range([
+         "#d4c685",
+         "#f7ef81",
+         "#cfe795",
+         "#a7d3a6",
+         "#add2c2",
+         "#d3ac00",
+         "#f7e600",
+         "#a2e800",
+         "#03d300",
+         "#00d176"
+       ]);
 
-    var pie = d3.pie().value(function(d) {
-      console.log(d);
-      console.log(d.value);
-      return d.value;
-    });
+     var pie = d3.pie().value(function(d) {
+       console.log(d);
+       console.log(d.value);
+       return d.value;
+     });
 
-    var arcGenerator = d3
-      .arc()
-      .innerRadius(0)
-      .outerRadius(radius);
+     var arcGenerator = d3
+       .arc()
+       .innerRadius(0)
+       .outerRadius(radius);
 
-    var data_ready = await pie(d3.entries(data));
-    console.log(data_ready);
+     var data_ready = await pie(d3.entries(data));
+     console.log(data_ready);
 
-    // Define the div for the tooltip
+
+     // Define the div for the tooltip
     var div = d3
       .select("#vis")
       .append("div")
       .attr("id", "tooltip")
       .style("opacity", 0);
-    svg
-      .selectAll("whatever")
-      .data(data_ready)
-      .enter()
-      .append("path")
-      .attr(
-        "d",
-        d3
-          .arc()
-          .innerRadius(radius - 75)
-          .outerRadius(radius)
-      )
-      .attr("fill", function(d) {
-        return color(d.data.key);
-      })
-      .attr("stroke", "black")
-      .style("stroke-width", "2px")
-      .style("opacity", 0.7)
-      .on("mouseover", function(d) {
-        div
-          .transition()
-          .duration(300)
-          .style("opacity", 1);
+     svg
+       .selectAll("whatever")
+       .data(data_ready)
+       .enter()
+       .append("path")
+       .attr(
+         "d",
+         d3
+           .arc()
+           .innerRadius(0)
+           .outerRadius(radius)
+       )
+       .attr("fill", function(d) {
+         console.log(d.data.key);
+         return color(d.data.key);
+       })
+       .attr("stroke", "black")
+       .style("stroke-width", "2px")
+       .style("opacity", 0.7)
+       .on("mouseover", function(d) {
+         console.log(d.data.key)
+         div.transition()
+          .duration(300) 
+          .style("opacity", 1)
 
-        div.html(
-          `<p><span class="bold">${d.data.key}</span> has <br/> ${d.data.value} commits</p>`
-        );
-      })
-      .on("mousemove", function(d) {
-        d3.select("#tooltip")
-          .style("top", d3.event.pageY - 10 + "px")
-          .style("left", d3.event.pageX + 10 + "px");
-      })
-      .on("mouseout", function() {
-        d3.select("#tooltip").style("opacity", 0);
-      });
+         div.html(`${d.data.key} has <br/> ${d.data.value} commits`)
+          
+          
+      
+       })
+       .on("mousemove", function(d) {
+         d3.select("#tooltip")
+           .style("top", d3.event.pageY - 10 + "px")
+           .style("left", d3.event.pageX + 10 + "px");
+       })
+       .on("mouseout", function() {
+         d3.select("#tooltip").style("opacity", 0);
+       });
 
-    svg
-      .selectAll("mySlices")
-      .data(data_ready)
-      .enter()
-      .append("text")
+     svg
+       .selectAll("mySlices")
+       .data(data_ready)
+       .enter()
+       .append("text")
       //  .text(function(d) {
       //    return d.data.key;
       //  })
@@ -250,20 +248,21 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
 
-  searchButton.addEventListener("click", ()=>{
+  searchButton.addEventListener("click", async ()=>{
     if(userInputValue.value !== ""){
       showData(); 
-
-      pieGenerator(repoData()); 
+      const data = await repoData(); 
+      pieGenerator(data); 
     }
   })
 
-  userInputValue.addEventListener("keydown", e => {
+  userInputValue.addEventListener("keydown", async e => {
     if(e.key === "Enter"){
       e.preventDefault(); 
       if (userInputValue.value !== "") {
         showData();
-        pieGenerator(repoData());
+        const data = await repoData(); 
+        pieGenerator(data);
       }
     }
   })
